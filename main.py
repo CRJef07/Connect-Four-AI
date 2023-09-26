3479b14c9c6bce459cf6c34ba4678a883299ee04
Jefté Andrés Vega Hidalgo <jefte.vega.hidalgo@est.una.ac.cr>
2023-09-25 20:59:46 -0600

    Update main.py
    CREAR FICHAS

--- main.py
+++ main.py
@@ -26,29 +26,25 @@ def winning_move(board, piece):
     # Check horizontal locations
     for c in range(COLS - 3):
         for r in range(ROWS):
-            if board[r][c] == piece and board[r][c + 1] == piece and board[r][c + 2] == piece and board[r][
-                c + 3] == piece:
+            if board[r][c] == piece and board[r][c + 1] == piece and board[r][c + 2] == piece and board[r][c + 3] == piece:
                 return True
 
     # Check vertical locations
     for c in range(COLS):
         for r in range(ROWS - 3):
-            if board[r][c] == piece and board[r + 1][c] == piece and board[r + 2][c] == piece and board[r + 3][
-                c] == piece:
+            if board[r][c] == piece and board[r + 1][c] == piece and board[r + 2][c] == piece and board[r + 3][c] == piece:
                 return True
 
     # Check positively sloped diagonals
     for c in range(COLS - 3):
         for r in range(ROWS - 3):
-            if board[r][c] == piece and board[r + 1][c + 1] == piece and board[r + 2][c + 2] == piece and board[r + 3][
-                c + 3] == piece:
+            if board[r][c] == piece and board[r + 1][c + 1] == piece and board[r + 2][c + 2] == piece and board[r + 3][c + 3] == piece:
                 return True
 
     # Check negatively sloped diagonals
     for c in range(COLS - 3):
         for r in range(3, ROWS):
-            if board[r][c] == piece and board[r - 1][c + 1] == piece and board[r - 2][c + 2] == piece and board[r - 3][
-                c + 3] == piece:
+            if board[r][c] == piece and board[r - 1][c + 1] == piece and board[r - 2][c + 2] == piece and board[r - 3][c + 3] == piece:
                 return True
 
 
@@ -125,13 +121,13 @@ def minimax_alpha_beta(board, depth, alpha, beta, maximizing_player):
     if depth == 0 or is_terminal:
         if is_terminal:
             if winning_move(board, 2):
-                return (None, 100000000000000)
+                return None, 100000000000000
             elif winning_move(board, 1):
-                return (None, -10000000000000)
+                return None, -10000000000000
             else:  # Game is over, no more valid moves
-                return (None, 0)
+                return None, 0
         else:  # Depth is zero
-            return (None, score_position(board, 2))
+            return None, score_position(board, 2)
 
     if maximizing_player:
         value = -np.Inf
@@ -176,7 +172,7 @@ def print_board(board):
     print()
 
 
-def play(board):
+def play(board, PLAYER_PIECES, AI_PIECES):
     while True:
         game_over = False
         turn = 0  # Player 1 starts
@@ -186,42 +182,46 @@ def play(board):
             if turn == 0:
                 col = int(input("Player 1, choose a column (1-7): "))
                 col = col - 1
+
                 if col > 6 or col < 0:
                     print("Error: La columna debe estar entre 1-7")
-                    return play(board)
-
+                    return play(board, PLAYER_PIECES, AI_PIECES)
 
                 if is_valid_location(board, col):
                     row = get_next_open_row(board, col)
                     drop_piece(board, row, col, 1)
+                    PLAYER_PIECES = PLAYER_PIECES - 1
 
                     if winning_move(board, 1):
                         print("Player 1 wins!")
                         game_over = True
                 else:
                     print("Error: La fila ya está llena")
-                    return play(board)
+                    return play(board, PLAYER_PIECES, AI_PIECES)
             else:
                 col, minimax_score = minimax_alpha_beta(board, 4, -np.Inf, np.Inf, True)
                 if is_valid_location(board, col):
                     row = get_next_open_row(board, col)
                     drop_piece(board, row, col, 2)
+                    AI_PIECES = AI_PIECES - 1
 
                     if winning_move(board, 2):
                         print("Player 2 (AI) wins!")
                         game_over = True
 
                 print_board(board)
+                print('PLAYER_PIECES: {}'.format(PLAYER_PIECES))
+                print('AI_PIECES: {}'.format(AI_PIECES))
+                print()
             turn = (turn + 1) % 2
-
-        play_again = input("Play again? (yes/no): ").lower()
-        if play_again != "yes":
-            break
+        break
 
 
 def main():
+    PLAYER_PIECES = 21
+    AI_PIECES = 21
     board = create_board()
-    play(board)
+    play(board, PLAYER_PIECES, AI_PIECES)
 
 
 if __name__ == "__main__":
